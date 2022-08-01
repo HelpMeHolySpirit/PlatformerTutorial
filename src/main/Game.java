305e9bf8375eac163b7f3cb9f79bb66abddf7f06
HelpMeHolySpirit <mz.piatkowski@gmail.com>
2022-08-01 12:45:02 +0200

    Complete Episode #01

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,4 +1,12 @@
 package main;
 
 public class Game {
+
+    private GameWindow gameWindow;
+    private GamePanel gamePanel;
+    public Game(){
+        gamePanel = new GamePanel();
+        gameWindow = new GameWindow(gamePanel);
+
+    }
 }
