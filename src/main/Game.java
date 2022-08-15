d62de978bbd6dec1bf86209650e628ac4e9da97b
HelpMeHolySpirit <mz.piatkowski@gmail.com>
2022-08-15 16:55:08 +0200

    Completed Episode #11

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,9 +1,11 @@
 package main;
 
-import entities.Entity;
 import entities.Player;
-import inputs.KeyboardInputs;
+import gamestates.Gamestate;
+import gamestates.Playing;
 import levels.LevelManager;
+
+import gamestates.Menu;
 
 import java.awt.*;
 
@@ -15,6 +17,8 @@ public class Game implements Runnable{
     private Thread gameThread;
     private final int FPS_SET = 120;
     private final int UPS_SET = 200;
+    private Playing playing;
+    private Menu menu;
     public final static int TILES_DEFAULT_SIZE = 32;
     public final static float SCALE = 1.0f;
     public final static int TILES_IN_WIDTH = 26;
@@ -38,9 +42,8 @@ public class Game implements Runnable{
     }
 
     private void initClasses() {
-        levelManager = new LevelManager(this);
-        player = new Player(200, 200, (int) (64 * SCALE), (int) (40 * SCALE));
-        player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
+        menu = new Menu(this);
+        playing =  new Playing(this);
     }
 
     private void startGameLoop(){
@@ -49,13 +52,30 @@ public class Game implements Runnable{
     }
 
     public void update() {
-        player.update();
-        levelManager.update();
+        switch(Gamestate.state){
+            case MENU:
+                menu.update();
+                break;
+            case PLAYING:
+                playing.update();
+                break;
+            default:
+                break;
+        }
     }
 
     public void render(Graphics g){
-        levelManager.draw(g);
-        player.render(g);
+
+        switch(Gamestate.state){
+            case MENU:
+                menu.draw(g);
+                break;
+            case PLAYING:
+                playing.draw(g);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -102,11 +122,16 @@ public class Game implements Runnable{
     }
 
     public void windowFocusLost(){
-        player.resetDirBooleans();
+        if(Gamestate.state == Gamestate.PLAYING){
+            playing.getPlayer().resetDirBooleans();
+        }
     }
 
-    public Player getPlayer(){
-        return player;
+    public Menu getMenu(){
+        return menu;
     }
 
+    public Playing getPlaying() {
+        return playing;
+    }
 }
